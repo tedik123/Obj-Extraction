fe1fd3d695a91fa94382ecf483001bba6512091f
Tedi K <tedi.krouchkov@du.edu>
2023-10-27 21:07:00 -0600

    QLabel scaling to grab pixel coordinates

--- pixels and uv stuff/GUI/CustomLabel.py
+++ pixels and uv stuff/GUI/CustomLabel.py
@@ -1,12 +1,11 @@
+import typing
 from typing import Optional
 
+from PyQt5 import QtCore, QtGui
 from PyQt5.QtCore import Qt, QPoint, QObject, pyqtSignal
 from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import QLabel, QApplication
 
-
-class LogObject(QObject):
-    MousePixmapSignal = pyqtSignal(QPoint, QColor)
 
 
 # https://gist.github.com/acbetter/32c575803ec361c3e82064e60db4e3e0
@@ -15,9 +14,6 @@ class LogObject(QObject):
 # and emit it when mouse moves
 class CustomLabel(QLabel):
     mouseMovePixelColor = pyqtSignal(QPoint, QColor)
-
-    # TODO converting QT coordinates to pixel coordinates with scaling consideration!
-    # https://stackoverflow.com/questions/59611751/pyqt5-image-coordinates
 
     # this might be useful?
     # https://stackoverflow.com/questions/69869064/how-to-get-pixel-location-and-draw-a-dot-on-that-location-using-pyqt5
@@ -38,19 +34,68 @@ class CustomLabel(QLabel):
         self.QImage = self.pixmap().toImage()
 
     def mouseMoveEvent(self, event):
-        point = event.pos()
+        point = self.getMousePositionOnImage(event.pos())
         color = QColor(self.QImage.pixel(point.x(), point.y()))
-        # self.log.MousePixmapSignal.emit(point, color)
         print("Moved!!", color.getRgb(), point)
-        QLabel.mouseMoveEvent(self, event)
         r, g, b, a = color.getRgb()
         self.mouseMovePixelColor.emit(point, QColor(r, g, b, a))
+        super().mouseMoveEvent(event)
 
     # this is the hover mouse event
     def enterEvent(self, event):
         QApplication.setOverrideCursor(Qt.CrossCursor)
-        QLabel.enterEvent(self, event)
+        super().enterEvent( event)
 
     def leaveEvent(self, event):
         QApplication.setOverrideCursor(Qt.ArrowCursor)
-        QLabel.leaveEvent(self, event)
+        super().leaveEvent( event)
+
+    def getMousePositionOnImage(self, pos) -> QtCore.QPoint or None:
+        # https://stackoverflow.com/questions/59611751/pyqt5-image-coordinates
+        # consider the widget contents margins
+        contentsRect = QtCore.QRectF(self.contentsRect())
+        if pos not in contentsRect:
+            # outside widget margins, ignore!
+            return
+
+        # adjust the position to the contents margins
+        pos -= contentsRect.topLeft()
+
+        pixmapRect = self.pixmap().rect()
+        if self.hasScaledContents():
+            x = pos.x() * pixmapRect.width() / contentsRect.width()
+            y = pos.y() * pixmapRect.height() / contentsRect.height()
+            print("X", x)
+            print("Y", y)
+            pos = QtCore.QPoint(int(x), int(y))
+        else:
+            align = self.alignment()
+            # for historical reasons, QRect (which is based on integer values),
+            # returns right() as (left+width-1) and bottom as (top+height-1),
+            # and so their opposite functions set/moveRight and set/moveBottom
+            # take that into consideration; using a QRectF can prevent that; see:
+            # https://doc.qt.io/qt-5/qrect.html#right
+            # https://doc.qt.io/qt-5/qrect.html#bottom
+            pixmapRect = QtCore.QRectF(pixmapRect)
+
+            # the pixmap is not left aligned, align it correctly
+            if align & QtCore.Qt.AlignRight:
+                pixmapRect.moveRight(contentsRect.x() + contentsRect.width())
+            elif align & QtCore.Qt.AlignHCenter:
+                pixmapRect.moveLeft(contentsRect.center().x() - pixmapRect.width() / 2)
+            # the pixmap is not top aligned (note that the default for QLabel is
+            # Qt.AlignVCenter, the vertical center)
+            if align & QtCore.Qt.AlignBottom:
+                pixmapRect.moveBottom(contentsRect.y() + contentsRect.height())
+            elif align & QtCore.Qt.AlignVCenter:
+                pixmapRect.moveTop(contentsRect.center().y() - pixmapRect.height() / 2)
+
+            if not pos in pixmapRect:
+                # outside image margins, ignore!
+                return
+            # translate coordinates to the image position and convert it back to
+            # a QPoint, which is integer based
+            pos = (pos - pixmapRect.topLeft()).toPoint()
+
+        print('X={}, Y={}'.format(pos.x(), pos.y()))
+        return pos
