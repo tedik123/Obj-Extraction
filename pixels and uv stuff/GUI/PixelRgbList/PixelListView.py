9dda90f15634930b0c5122032fbfd4ba8d3ff1c0
Tedi K <tedi.krouchkov@du.edu>
2023-11-04 13:36:24 -0600

    work in progress for MVC and generated pyqt5 files

--- pixels and uv stuff/GUI/PixelRgbList/PixelListView.py
+++ pixels and uv stuff/GUI/PixelRgbList/PixelListView.py
@@ -5,17 +5,17 @@ from PyQt5.QtWidgets import QScrollArea, QWidget, QVBoxLayout, QLabel, QSizePoli
 class PixelList(QScrollArea):
     def __init__(self, parent=None):
         super().__init__(parent)
-
-        example_widget = QWidget()
-        self.setWidget(example_widget)
+        # I Don't know if I even need this
+        self.main_widget = QWidget()
+        self.setWidget(self.main_widget)
 
         # set the scroll area so it vertically allows for scrolling
         self.setWidgetResizable(True)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 
-        vertical_layout = QVBoxLayout()
-        example_widget.setLayout(vertical_layout)
+        self.vertical_layout = QVBoxLayout()
+        self.main_widget.setLayout(self.vertical_layout)
         example_label = QLabel()
         example_label.setText("Something random and long 1")
         # second example label widget
@@ -25,9 +25,7 @@ class PixelList(QScrollArea):
         for i in range(100):
             example_label = QLabel()
             example_label.setText("Something random and long " + str(i))
-            vertical_layout.insertWidget(0, example_label)
-        # vertical_layout.insertWidget(0, example_label2)
-        # vertical_layout.insertWidget(0, example_label)
+            self.vertical_layout.insertWidget(0, example_label)
         # this pushes everything to the top
-        vertical_layout.addStretch()
+        self.vertical_layout.addStretch()
         self.show()
